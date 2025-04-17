cbbef51bb7511f7b55fad338902cc4db04ff5a23
Sergio Méndez <sergiojoel50@hotmail.com>
2025-04-16 21:39:50 -0600

    Practica 5

--- src/main/java/com/tienda/service/ReporteService.java
+++ src/main/java/com/tienda/service/ReporteService.java
@@ -1,20 +1,14 @@
-
-
 package com.tienda.service;
 
 import java.io.IOException;
+import java.util.Map;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import java.util.Map;
-
 
 public interface ReporteService {
-
-    public ResponseEntity<Resource> generaReporte(
-    String reporte,
-            Map<String, Object> parametros,
-            String tipo
-            ) 
-            throws IOException;
     
+    ResponseEntity<Resource> generaReporte(
+            String reporte,
+            Map<String, Object> parametros,
+            String tipo) throws IOException;
 }
